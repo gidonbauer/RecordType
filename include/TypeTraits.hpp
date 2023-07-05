c33a2cc4b732bbf619b80268b4b2dfbb0407393e
gidon.bauer <gidon.bauer@hotmail.de>
2023-07-05 11:45:29 +0200

    Simplify TypeTraits

--- include/TypeTraits.hpp
+++ include/TypeTraits.hpp
@@ -9,9 +9,9 @@
 namespace RT {
 
 // - Check for RecordType --------------------------------------------------------------------------
-template <typename>
+template <typename T>
 struct is_record_type : std::false_type {
-  using underlying_type = void;
+  using underlying_type = T;
 };
 
 template <typename T>
@@ -23,23 +23,8 @@ template <typename T>
 constexpr bool is_record_type_v = is_record_type<T>::value;
 
 // - Decay RecordType to underlying type -----------------------------------------------------------
-namespace internal {
-
 template <typename T>
-[[noreturn]] constexpr auto decay_record_type() -> T {
-  RT_PANIC("This function should never be called. Use the type `decay_record_type`.");
-}
-
-template <typename T>
-requires is_record_type_v<T>
-[[noreturn]] constexpr auto decay_record_type() -> typename is_record_type<T>::underlying_type {
-  RT_PANIC("This function should never be called. Use the type `decay_record_type`.");
-}
-
-}  // namespace internal
-
-template <typename T>
-using decay_record_type_t = decltype(internal::decay_record_type<T>());
+using decay_record_type_t = typename is_record_type<T>::underlying_type;
 
 }  // namespace RT
 
