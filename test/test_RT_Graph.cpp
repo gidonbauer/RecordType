84615548e0a823ddf387a0e4ddff49402178bab3
gidon.bauer <gidon.bauer@hotmail.de>
2023-07-24 22:40:15 +0200

    Add test for case that RecordTypes are in different graphs

--- test/test_RT_Graph.cpp
+++ test/test_RT_Graph.cpp
@@ -3,8 +3,54 @@
 #include "Graph.hpp"
 #include "RecordType.hpp"
 
+TEST(test_RT_Graph, DifferentGraphs) {
+  using PT    = int;
+  using RType = RT::RecordType<PT>;
+
+  auto graph1 = std::make_shared<RT::Graph<PT>>();
+  const RType rt1(42);
+  rt1.register_graph(graph1);
+
+  auto graph2 = std::make_shared<RT::Graph<PT>>();
+  const RType rt2(-42);
+  rt2.register_graph(graph2);
+
+  const RType rt3 = rt1 + rt2;
+
+  EXPECT_NE(rt1.id(), RT::UNREGISTERED) << "rt1 was not properly registered";
+  EXPECT_NE(rt2.id(), RT::UNREGISTERED) << "rt2 was not properly registered";
+  EXPECT_EQ(rt3.id(), RT::UNREGISTERED) << "rt3 should not have been registered";
+
+  EXPECT_NE(rt1.graph(), nullptr) << "rt1 should have a graph";
+  EXPECT_NE(rt2.graph(), nullptr) << "rt2 should have a graph";
+  EXPECT_NE(rt1.graph(), rt2.graph()) << "rt1 and rt2 should be in different graphs";
+
+  EXPECT_EQ(rt3.graph(), nullptr) << "rt3 should not have a graph";
+
+  EXPECT_EQ(rt3.value(), 0);
+
+  ASSERT_EQ(graph1->dependencies().size(), 1ul);
+  EXPECT_EQ(graph1->dependencies()[0], rt1.id());
+
+  ASSERT_EQ(graph1->values().size(), 1ul);
+  EXPECT_EQ(graph1->values()[0], 42);
+
+  ASSERT_EQ(graph1->operations().size(), 1ul);
+  EXPECT_EQ(graph1->operations()[0], RT::NodeType::VAR);
+
+  ASSERT_EQ(graph2->dependencies().size(), 1ul);
+  EXPECT_EQ(graph2->dependencies()[0], rt2.id());
+
+  ASSERT_EQ(graph2->values().size(), 1ul);
+  EXPECT_EQ(graph2->values()[0], -42);
+
+  ASSERT_EQ(graph2->operations().size(), 1ul);
+  EXPECT_EQ(graph2->operations()[0], RT::NodeType::VAR);
+}
+
 TEST(test_RT_Graph, SimpleAddition) {
   using Rec_t = RT::RecordType<int>;
+
   const Rec_t rt0(42);
   const Rec_t rt1(-42);
 
