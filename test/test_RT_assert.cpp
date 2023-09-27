0210fab8c63880f5de5cb2d1596d234b01e66614
gidon.bauer <gidon.bauer@hotmail.de>
2023-09-27 23:22:06 +0200

    Fix test for assert

--- test/test_RT_assert.cpp
+++ test/test_RT_assert.cpp
@@ -29,19 +29,19 @@ TEST(TestRTAssert, ShouldFail) {
   EXPECT_EXIT(
       RT_ASSERT(i * d < 12.0, "3 * 4.2 is greater than 12");,
       ::testing::KilledBySignal(SIGABRT),
-      ".+[ASSERT].+:30.+`i \\* d < 12.0` failed: 3 \\* 4.2 is greater than 12\n"
+      ".+[ASSERT].+`i \\* d < 12.0` failed: 3 \\* 4.2 is greater than 12\n"
   );
 
   EXPECT_EXIT(
       RT_ASSERT(i * d > 13.0, "3 * 4.2 is less than 13");,
       ::testing::KilledBySignal(SIGABRT),
-      ".+[ASSERT].+:36.+`i \\* d > 13.0` failed: 3 \\* 4.2 is less than 13\n"
+      ".+[ASSERT].+`i \\* d > 13.0` failed: 3 \\* 4.2 is less than 13\n"
   );
 
   EXPECT_EXIT(
       RT_ASSERT((std::is_same_v<decltype(i * d), int>), "Type of expression should be double");,
       ::testing::KilledBySignal(SIGABRT),
-      ".+[ASSERT].+:42.+`\\(std::is_same_v<decltype\\(i \\* d\\), int>\\)` failed: Type of expression should be double\n"
+      ".+[ASSERT].+`\\(std::is_same_v<decltype\\(i \\* d\\), int>\\)` failed: Type of expression should be double\n"
   );
   // clang-format on
 }
